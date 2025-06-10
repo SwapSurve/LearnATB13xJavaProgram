38389f1174bbedd0fa53a57b52ffbec4e6982142
SwapSurve <surve.swapil2021@gmail.com>
2025-06-10 23:17:05 +0530

    10 June

---
+++ src/Lab0022.java
@@ -0,0 +1,19 @@
+
+public class Lab0022
+{
+    public static void main(String[] args)
+    {
+       int a=11;
+
+        System.out.println(a);
+        a=60+1;
+        System.out.println(a);
+
+        int age=21;
+        age=age+1;
+        age=45;
+        System.out.println(age);
+
+        System.out.printf("Your Variable name %d", age);
+    }
+}
